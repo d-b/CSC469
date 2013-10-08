5741c4a31a34f7eae0849d5500f623454ea78f1a
Daniel Bloemendal <d.bloemendal@gmail.com>
2013-10-08 04:06:21 -0400

    added clockrate test

--- A1/src/perftest.cpp
+++ A1/src/perftest.cpp
@@ -6,6 +6,11 @@
 
 #include "perftest.hpp"
 
+#include <iostream>
+
 int main(int argc, char* argv[]) {
-    return 0;
+    Clockrate clockrate(4, 0.25);
+    for(int i = 0; i < 4; i++)
+        clockrate.sample();
+    std::cout << clockrate.rate()/1e+9 << "GHz" << std::endl;
 }
