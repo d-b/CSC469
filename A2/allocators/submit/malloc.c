ed22daa7eb0cee228d19b3510c97ebb1c7dbe8e7
Daniel Bloemendal <d.bloemendal@gmail.com>
2013-11-10 21:48:25 -0500

    progress on malloc

--- A2/allocators/submit/malloc.c
+++ A2/allocators/submit/malloc.c
@@ -16,7 +16,6 @@
 //
 // Hoard parameters
 //
-#define ALLOC_HOARD_EMPTY_FRACTION  0.25
 #define ALLOC_HOARD_FULLNESS_GROUPS 4
 #define ALLOC_HOARD_SIZE_CLASS_BASE 2
 #define ALLOC_HOARD_SIZE_CLASS_MIN  2
@@ -26,6 +25,16 @@
 // Hoard structures
 //
 
+// Forward declarations
+struct SUPERBLOCK_T;
+struct HEAP_T;
+struct CONTEXT_T;
+
+// Types
+typedef struct SUPERBLOCK_T superblock_t;
+typedef struct HEAP_T heap_t;
+typedef struct CONTEXT_T context_t;
+
 // Invalid block
 enum E_BLOCK { BLOCK_INVALID = -1 };
 
@@ -33,31 +42,33 @@ enum E_BLOCK { BLOCK_INVALID = -1 };
 typedef int32_t blockptr_t;
 
 // Superblock structure
-typedef struct SUPERBLOCK_T {
-    int size_class;
-    size_t block_size;
-    size_t block_count;
-    size_t block_used;
-    blockptr_t next_block;
-    blockptr_t next_free;
-    struct SUPERBLOCK_T* prev;
-    struct SUPERBLOCK_T* next;
-} superblock_t;
+struct SUPERBLOCK_T {
+    heap_t*         heap;
+    int             size_class;
+    size_t          block_size;
+    size_t          block_count;
+    size_t          block_used;
+    blockptr_t      next_block;
+    blockptr_t      next_free;
+    superblock_t*   prev;
+    superblock_t*   next;
+};
 
 // Heap
-typedef struct {
+struct HEAP_T {
     pthread_mutex_t lock;
-    size_t mem_used;
-    size_t mem_allocated;
-    superblock_t* bins[ALLOC_HOARD_FULLNESS_GROUPS];
-} heap_t;
+    int             index;
+    size_t          mem_used;
+    size_t          mem_allocated;
+    superblock_t*   bins[ALLOC_HOARD_FULLNESS_GROUPS];
+};
 
 // Allocator context
-typedef struct {
-    void*  blocks_base;
-    int    heap_count;
-    heap_t heap_table[1];
-} context_t;
+struct CONTEXT_T {
+    void*           blocks_base;
+    int             heap_count;
+    heap_t          heap_table[1];
+};
 
 //
 // Utility functions
@@ -91,7 +102,7 @@ inline size_t superblock_size(void) {
     return mem_pagesize();
 }
 
-static void superblock_init(superblock_t* sb, int size_class) {
+static void superblock_init(superblock_t* sb, heap_t* heap, int size_class) {
     // Set size class
     sb->size_class = size_class;
 
@@ -107,6 +118,7 @@ static void superblock_init(superblock_t* sb, int size_class) {
     sb->next_free   = BLOCK_INVALID;
     
     // Link entries
+    sb->heap = heap;
     sb->next = NULL;
     sb->prev = NULL;
 }
@@ -142,6 +154,9 @@ static blockptr_t superblock_block_allocate(superblock_t* sb) {
         return BLOCK_INVALID;
     sb->block_used += 1;
 
+    // Update heap statistics
+    sb->heap->mem_used += sb->block_size;
+
     // Return a new or freed block
     blockptr_t blk = superblock_freelist_pop(sb);
     return (blk != BLOCK_INVALID) ? blk : sb->next_block++;
@@ -152,6 +167,9 @@ static void superblock_block_free(superblock_t* sb, blockptr_t blk) {
     assert(sb->block_used > 0);
     superblock_freelist_push(sb, blk);
     sb->block_used -= 1;
+
+    // Update heap statistics
+    sb->heap->mem_used -= sb->block_size;
 }
 
 //
@@ -162,8 +180,14 @@ static void heap_init(heap_t* heap) {
     pthread_mutex_init(&heap->lock, NULL);
     heap->mem_used = 0;
     heap->mem_allocated = 0;
-    int i; for(i = 0; i < ALLOC_HOARD_FULLNESS_GROUPS; i++)
+    int i; for(i = 0; i < ALLOC_HOARD_FULLNESS_GROUPS; i++) {
+        heap->index = i;
         heap->bins[i] = NULL;
+    }
+}
+
+static int heap_full(heap_t* heap) {
+    return heap->mem_used >= heap->mem_allocated;
 }
 
 //
@@ -171,19 +195,62 @@ static void heap_init(heap_t* heap) {
 //
 
 inline static size_t context_size() {
-    size_t size = sizeof(context_t) + sizeof(heap_t) * (getNumProcessors() * ALLOC_HOARD_HEAP_CPU_FACTOR - 1);
+    size_t size = sizeof(context_t) + sizeof(heap_t) * getNumProcessors() * ALLOC_HOARD_HEAP_CPU_FACTOR;
     return util_pagealigned(size);
 }
 
 static void context_init(context_t* ctx) {
     ctx->blocks_base = (char*) ctx + context_size();
     ctx->heap_count = getNumProcessors() * ALLOC_HOARD_HEAP_CPU_FACTOR;
-    int i; for(i = 0; i < ctx->heap_count; i++)
+    heap_init(&ctx->heap_table[0]);
+    int i; for(i = 1; i < ctx->heap_count; i++)
         heap_init(&ctx->heap_table[i]);
 }
 
+static heap_t* context_globalheap(context_t* ctx) {
+    return &ctx->heap_table[0];
+}
+
 static heap_t* context_heap(context_t* ctx, uint32_t threadid) {
-    return &ctx->heap_table[threadid % ctx->heap_count];
+    return &ctx->heap_table[1 + (threadid % ctx->heap_count)];
+}
+
+static void* context_malloc(context_t* ctx, heap_t* heap, size_t sz) {
+    // Fetch global heap
+    heap_t* glob = context_globalheap(ctx);
+
+    // Compute size class
+    int sizecls = util_sizeclass(sz);
+
+    // Scan heap for appropriate superblock
+    superblock_t* sb = NULL; int group;
+    for(group = ALLOC_HOARD_FULLNESS_GROUPS; group >= 1; group--) {
+        for(sb = heap->bins[group]; sb; sb = sb->next)
+            if(sb->size_class == sizecls &&
+               sb->block_used <  sb->block_count) break;
+        // If block was found break out
+        if(sb) break;
+    }
+
+    // If an appropriate superblock was found
+    if(sb) {
+        // Allocate a new block
+        blockptr_t blk = superblock_block_allocate(sb);
+
+        // See if we need to move the superblock to a new fullness group
+        // ...
+
+        // Return the address of the block in memory
+        return superblock_block_data(sb, blk);
+    }
+    // If there are blocks available in the global heap
+    else if(!heap_full(glob)) {
+        // ...
+    }
+    // Otherwise allocate a new superblock
+    else {
+        // ...
+    }
 }
 
 
@@ -199,24 +266,25 @@ void *mm_malloc(size_t sz)
 {
     // See if we are allocating from system
     if(sz > superblock_size()/2)
-        return mem_sbrk(util_pagealigned(sz));
+        return malloc(sz);
+
+    // The result
+    void* mem = NULL;
 
     // Get context
     context_t* ctx = get_context();
 
-    // Find current heap and lock it
+    // Find current heap and lock it & allocate memory
     heap_t* heap = context_heap(ctx, (uint32_t) pthread_self());
     pthread_mutex_lock(&heap->lock);
-
-    // Unlock and return
+        mem = context_malloc(ctx, heap, sz);
     pthread_mutex_unlock(&heap->lock);
-    return NULL;
-
+    return mem;
 }
 
 void mm_free(void *ptr)
 {
-	(void)ptr; /* Avoid warning about unused variable */
+    (void)ptr; /* Avoid warning about unused variable */
 }
 
 
@@ -229,5 +297,5 @@ int mm_init(void)
     context_t* ctx = mem_sbrk(context_size());
     if(ctx == NULL) return -1;
     context_init(ctx);
-	return 0;
+    return 0;
 }
